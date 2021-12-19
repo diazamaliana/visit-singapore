ac1e6a005886d8f6e4e27ee50e75089a6f3424d0
Diaz Amaliana <diazamaliana@gmail.com>
2021-12-19 15:14:35 +0700

    feat: add data

---
+++ src/data/markers.js
@@ -0,0 +1,110 @@
+export const markers = [
+    {
+      id: 1,
+      position: {
+        lat: 1.286920,
+        lng: 103.854570,
+      },
+      name: "Merlion",
+      address: "1 Fullerton Rd, Singapore",
+      description: "Travellers acquainted with Singapore will probably already know of our city's national icon—the mythical Merlion, which possesses the body of a fish and the head of a lion. One of the most famous depictions of this iconic symbol is the statue found at Merlion Park—a scenic spot that stands near the mouth of the Singapore River.",
+      image: "https://images.unsplash.com/photo-1615916884911-2efa7ce4eaa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+      link: "https://marinabaysands.com/"
+    },
+    {
+      id: 2,
+      position: {
+        lat: 1.283099,
+        lng: 103.860295,
+      },
+      name: "Marina Bay Sands",
+      address: "10 Bayfront Ave, Singapore",
+      description: "Marina Bay Sands is one of two winning proposals for Singapore's first integrated resorts, the other being the Resorts World Sentosa, which incorporates a family-friendly Universal Studios Theme Park (Universal Studios Singapore).",
+      image: "https://images.unsplash.com/photo-1599594026550-97d9565ceda3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+      link: "https://www.marinabaysands.com/"
+    },
+    {
+      id: 3,
+      position: {
+        lat: 1.281790,
+        lng: 103.863954,
+      },
+      name: "Gardens By The Bay",
+      address: "18 Marina Gardens Dr, Singapore",
+      description: "There’s always something blooming in the Gardens. Spend an afternoon with unusual plants from around the world in the cooled conservatories, swing by the OCBC Skyway for spectacular views from 22m above ground, and check out our ongoing programmes for a fun-filled day for the entire family!",
+      image: "https://images.unsplash.com/photo-1558289675-f8a783516e7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      link: "https://www.gardensbythebay.com.sg/"
+    },
+    {
+      id: 4,
+      position: {
+        lat: 1.284193,
+        lng: 103.843362,
+      },
+      name: "Chinatown",
+      address: "Central Region",
+      description: "Once an enclave for Singapore’s Chinese immigrant population, the Chinatown of today is much-beloved for its blend of old and new, with historic temples and traditional medicinal halls sitting alongside bold new bars and trendy lifestyle shops.",
+      image: "https://images.unsplash.com/photo-1621395731232-ca7108eec330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1141&q=80",
+      link: "https://chinatown.sg/"
+    },
+    {
+      id: 5,
+      position: {
+        lat: 1.287466,
+        lng: 103.851424,
+      },
+      name: "Asian Civilisations Museum",
+      address: "1 Empress Pl, Singapore",
+      description: "With its mix of races and cultures, Singapore has rightfully earned a reputation for being the cultural melting pot of Asia. To showcase this diverse heritage, the Asian Civilisations Museum (ACM) takes you on a grand exploration of the people from around the region that have settled on our shores over the past two centuries. ",
+      image: "https://ik.imagekit.io/tvlk/xpe-asset/AyJ40ZAo1DOyPyKLZ9c3RGQHTP2oT4ZXW+QmPVVkFQiXFSv42UaHGzSmaSzQ8DO5QIbWPZuF+VkYVRk6gh-Vg4ECbfuQRQ4pHjWJ5Rmbtkk=/2000808570217/Asian%2520Civilisations%2520Museum%2520Tickets-31ffbc64-154b-4b59-a4d1-0c6a2c4a1185.jpeg?_src=imagekit&tr=c-at_max,h-512,q-60,w-720",
+      link: "https://www.nhb.gov.sg/acm/"
+    },
+    {
+      id: 6,
+      position: {
+        lat: 1.290555,
+        lng: 103.846188,
+      },   
+      name: "Clarke Quay",
+      address: "3 River Valley Rd, Singapore",
+      description: "From iconic dance clubs to live music venues and rooftop bars, this ultimate guide to drinking and partying in both Clarke Quay and Boat Quay is a must-have for anyone looking for a boozy good time in the city.",
+      image: "https://images.unsplash.com/photo-1624347648284-d7627b1381fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      link: "https://marinabaysands.com/"
+    },
+    {
+      id: 7,
+      position: {
+        lat: 1.295526,
+        lng: 103.845331,
+      },
+      name: "Fort Canning Park",
+      address: "River Valley Rd, Singapore",
+      description: "Imagine a place in Singapore where Malay royalty once ruled in medieval times, where the British decided to surrender to the invading Japanese during World War II (WWII), and more recently, some of the world’s biggest music acts played to rapt audiences.",
+      image: "https://ik.imagekit.io/tvlk/xpe-asset/dsIfD0QxFcgaDmB6sQchobk5CmBu9PzsWhwFXGFxJ179jzSxIGG5kZNhhHY-p7nw/xxt/experience/image/Fort-Canning-Park-Tour-04-jpg-960x720-FIT-76a6aa223443ad6682bf82917c750e6d.jpeg?_src=imagekit&tr=c-at_max,h-500,q-60,w-612",
+      link: "https://www.nparks.gov.sg/"
+    },
+    {
+      id: 8,
+      position: {
+        lat: 1.289332,
+        lng: 103.863152,
+      },
+      name: "Singapore Flyer",
+      address: "30 Raffles Ave, Singapore",
+      description: "A giant observation wheel that stands amidst the skyscrapers in the Singapore skyline, the Singapore Flyer is the go-to attraction for the most magnificent views of our city. A scenic spin on the Singapore Flyer takes you 165 metres above ground, or as high as 42 storeys, as you marvel at the spectacular scenery below.",
+      image: "https://images.unsplash.com/photo-1559633462-d82e21578552?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      link: "https://www.singaporeflyer.com/en"
+    },
+    {
+      id: 9,
+      position: {
+        lat: 1.302279,
+        lng: 103.837399,
+      },
+      name: "Orchard Road",
+      address: "Central Region",
+      description: "Orchard Road’s humble beginnings date back to the early 1830s, when it was an unnamed road home to fruit orchards, nutmeg plantations and pepper farms. In 1958, Orchard Road’s first department store—TANGS—was set up by local merchant C.K. Tang. Then known as House of Tangs, it marked the neighbourhood’s first step towards becoming Singapore’s most beloved shopping district.",
+      image: "https://images.unsplash.com/photo-1574489434031-9d7186d52eed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      link: "https://www.orchardroad.org/"
+    },
+];
---
+++ src/data/menu.js
@@ -0,0 +1,27 @@
+import attractionIcon from 'assets/icons/attraction-icon.svg';
+import blogIcon from 'assets/icons/blog-icon.svg';
+import videosIcon from 'assets/icons/videos-icon.svg';
+import aboutIcon from 'assets/icons/about-icon.svg';
+
+export const menu = [
+    {
+        id: 2,
+        title: 'Suggest Attraction',
+        src: attractionIcon,
+    },
+    {
+        id: 3,
+        title: 'Videos',
+        src: blogIcon,
+    },
+    {
+        id: 4,
+        title: 'Blog',
+        src: videosIcon,
+    },
+    {
+        id: 5,
+        title: 'About',
+        src: aboutIcon,
+    },
+  ];
